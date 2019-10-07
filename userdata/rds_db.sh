6237dc797d8de09003a143761f9bb1f44b78a51c
Andriy Humenetskyy <ahumenetskyy@lohika.com>
2019-10-07 11:24:59 +0300

    update connection data for hw4

--- userdata/rds_db.sh
+++ userdata/rds_db.sh
@@ -5,7 +5,7 @@ sudo yum -y group install "Development Tools"
 
 # Setup App
 sudo yum install -y python3 python3-devel git postgresql-devel
-sudo git clone https://github.com/horakihor/aws_course.git /opt/aws_course && \
+sudo git clone https://github.com/ahumenetskyy/aws_course.git /opt/aws_course && \
 cd /opt/aws_course/app/ && \
 sudo make setup && \
-sudo make run POSTGRES_URL="<RDS-HOSTNAME>:5432"
+sudo make run POSTGRES_URL="10.214.128.3:5432" POSTGRES_PW="udD7tdGyKlnLuiaK" POSTGRES_DB="hw4"
